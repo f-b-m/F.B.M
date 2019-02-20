aebb86091d6b946231882797a5e6e2c36d82a19b
dondakeshimo <went.went.takkun135@gmail.com>
2019-02-21 01:25:36 +0900

    [fix]remove unuse button

--- app/javascript/components/TooltipsAddButton.jsx
+++ app/javascript/components/TooltipsAddButton.jsx
@@ -3,8 +3,6 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AddIcon from '@material-ui/icons/Add';
 import Fab from '@material-ui/core/Fab';
-import DeleteIcon from '@material-ui/icons/Delete';
-import IconButton from '@material-ui/core/IconButton';
 import Tooltip from '@material-ui/core/Tooltip';
 
 const styles = theme => ({
@@ -22,16 +20,6 @@ function SimpleTooltips(props) {
   const { classes } = props;
   return (
     <div>
-      <Tooltip title="Delete">
-        <IconButton aria-label="Delete">
-          <DeleteIcon />
-        </IconButton>
-      </Tooltip>
-      <Tooltip title="Add" aria-label="Add">
-        <Fab color="primary" className={classes.fab}>
-          <AddIcon />
-        </Fab>
-      </Tooltip>
       <Tooltip title="Add" aria-label="Add">
         <Fab color="secondary" className={classes.absolute}>
           <AddIcon />
