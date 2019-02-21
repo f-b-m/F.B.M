9b91a0a4d009c63b2a89ac3a8504b90a05f8bc40
dondakeshimo <went.went.takkun135@gmail.com>
2019-02-21 11:58:17 +0900

    [add]add Modal when click on edit button

--- app/javascript/components/TooltipsAddButton.jsx
+++ app/javascript/components/TooltipsAddButton.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Icon from '@material-ui/core/Icon';
+import Typography from '@material-ui/core/Typography';
+import EditIcon from '@material-ui/icons/Edit';
 import Fab from '@material-ui/core/Fab';
 import Tooltip from '@material-ui/core/Tooltip';
+import Modal from '@material-ui/core/Modal';
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
 
 const styles = theme => ({
   fab: {
@@ -14,19 +27,62 @@ const styles = theme => ({
     bottom: theme.spacing.unit * 2,
     right: theme.spacing.unit * 3,
   },
+  paper: {
+    position: 'absolute',
+    width: theme.spacing.unit * 50,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing.unit * 4,
+    outline: 'none',
+  },
 });
 
-function SimpleTooltips(props) {
-  const { classes } = props;
-  return (
-    <div>
-      <Tooltip title="Add" aria-label="Add">
-        <Fab color="secondary" className={classes.absolute}>
-          <Icon>edit_icon</Icon>
-        </Fab>
-      </Tooltip>
-    </div>
-  );
+class SimpleTooltips extends React.Component {
+  state = {
+    open: false,
+  };
+
+  handleOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { open } = this.state;
+
+    return (
+      <div>
+        <Tooltip title="Edit" aria-label="Edit">
+          <Fab
+            onClick={this.handleOpen}
+            color="secondary"
+            className={classes.absolute}
+          >
+            <EditIcon />
+          </Fab>
+        </Tooltip>
+        <Modal
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+          open={open}
+          onClose={this.handleClose}
+        >
+          <div style={getModalStyle()} className={classes.paper}>
+            <Typography variant="h6" id="modal-title">
+              Text in a modal
+            </Typography>
+            <Typography variant="subtitle1" id="simple-modal-description">
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+          </div>
+        </Modal>
+      </div>
+    );
+  }
 }
 
 SimpleTooltips.propTypes = {
