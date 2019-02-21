a9d6a795516a36c25dc65c0c9bfa501b25bcc76f
fujiki <tengmuyou0919@gmail.com>
2019-02-21 19:01:17 +0900

    [fix]routingの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {sessions: 'users/sessions', registrations: 'users/registrations'}
+  devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations'}
   root to: 'menus#menu'
 
   get 'menus/index', to: 'menus#index'
