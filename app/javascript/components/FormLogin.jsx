74e273db2ea1208d209b98ce928abb7f940cde11
dondakeshimo <went.went.takkun135@gmail.com>
2019-02-21 13:23:35 +0900

    [add]add input form for login

---
+++ app/javascript/components/FormLogin.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+  },
+});
+
+function FilledTextFields(props) {
+  const { classes } = props;
+
+  return (
+    <form className={classes.container} noValidate autoComplete="off">
+      <TextField
+        id="filled-email-input"
+        label="Email"
+        className={classes.textField}
+        type="email"
+        name="email"
+        autoComplete="email"
+        margin="normal"
+        variant="filled"
+      />
+
+      <TextField
+        id="filled-password-input"
+        label="Password"
+        className={classes.textField}
+        type="password"
+        autoComplete="current-password"
+        margin="normal"
+        variant="filled"
+      />
+    </form>
+  );
+}
+
+FilledTextFields.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(FilledTextFields);
