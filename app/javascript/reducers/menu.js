f9589aaa57c8e705e8f06f4ee06edad9df9e0e07
dondakeshimo <went.went.takkun135@gmail.com>
2019-02-22 16:29:38 +0900

    [add]add redux menu reducer

---
+++ app/javascript/reducers/menu.js
@@ -0,0 +1,28 @@
+import { menuActionType } from '../actions';
+
+const menus = (state = [], action) => {
+  switch (action.type) {
+    case menuActionType.add:
+      return [
+        ...state,
+        {
+          menuId: action.menuId,
+          title: action.title,
+          content: action.content,
+        },
+      ];
+
+    // TODO: implement logic
+    case menuActionType.edit:
+      return state;
+
+    // TODO: implement logic
+    case menuActionType.delete:
+      return state;
+
+    default:
+      return state;
+  }
+};
+
+export default menus;
