301ebcc9f72aaba2850f067663755e6cb428d56f
dondakeshimo <went.went.takkun135@gmail.com>
2019-02-22 15:15:42 +0900

    [fix]make only one parent component to conbine body and header

--- app/javascript/packs/menu.jsx
+++ app/javascript/packs/menu.jsx
@@ -1,15 +1,7 @@
 /* eslint no-console:0 */
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Header from '../components/Header'
 import Body from '../components/Body'
-
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <Header />,
-    document.querySelector('#header')
-  )
-});
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
@@ -17,10 +9,3 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#app')
   )
 });
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   ReactDOM.render(
-//     <div>Yu Fujiki & Taku SHimomura</div>,
-//     document.querySelector('#footer')
-//   )
-// });
