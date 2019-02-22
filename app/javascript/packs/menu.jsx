b3a2f5b6ada0242c570c54487f1a0a00160e8d30
dondakeshimo <went.went.takkun135@gmail.com>
2019-02-22 15:25:15 +0900

    [add]add redux store on root component

--- app/javascript/packs/menu.jsx
+++ app/javascript/packs/menu.jsx
@@ -1,11 +1,18 @@
 /* eslint no-console:0 */
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducer';
 import Body from '../components/Body'
+
+const store = createStore(rootReducer);
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Body />,
+    <Provider store={store}>
+      <Body />
+    </Provider>,
     document.querySelector('#app')
   )
 });
